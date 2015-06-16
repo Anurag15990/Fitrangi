062d1d0d8511c911f21591f871b37ffb83f236f5
Toovia <toovia@Toovias-MacBook-Pro.local>
2015-06-16 13:04:48 +0530

    Changes

--- Fitrangi/Fitrangi/Stream/StreamFollowCard.swift
+++ Fitrangi/Fitrangi/Stream/StreamFollowCard.swift
@@ -12,6 +12,7 @@ class StreamFollowCard: UITableViewCell {
     
     @IBOutlet weak var followerProfileImageView : AsyncImageView!
     @IBOutlet weak var followingProfileImageView : AsyncImageView!
+    @IBOutlet weak var followingCoverImageView : AsyncImageView!
     @IBOutlet weak var followersName : UILabel!
     @IBOutlet weak var followingName : UILabel!
     @IBOutlet weak var actionLabel : UILabel!
@@ -27,6 +28,7 @@ class StreamFollowCard: UITableViewCell {
         actionLabel.text = "Follows"
         followerProfileImageView.setImageWithUrl("http://www.fitrangi.com/assets/media/profile/556f0ede453d1e5570a1636a/cover.jpeg", options: AsyncImageOptions.ShowAlways)
         followingProfileImageView.setImageWithUrl("http://www.fitrangi.com/assets/media/profile/552e3f3f453d1e2b529d6518/cover.png", options: AsyncImageOptions.ShowAlways)
+        followingCoverImageView.setImageWithUrl("http://d3q5zq83v3a4xj.cloudfront.net/images/userprofile-banner.jpg", options: AsyncImageOptions.ShowAlways)
         // Initialization code
     }
 
